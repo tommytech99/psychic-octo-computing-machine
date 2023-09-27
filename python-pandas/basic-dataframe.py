11b63ba6d73deada62705f9c7d824eda590a01a3
Tommy <thomase.blanchard@yahoo.com>
2023-09-27 16:56:03 -0500

    Adding dataframe from AI generation

---
+++ python-pandas/basic-dataframe.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+# Create a dictionary with the data
+data = {
+    'Name': ['John', 'Jane', 'Mike'],
+    'Age': [25, 30, 35],
+    'City': ['New York', 'London', 'Paris']
+}
+
+# Create the DataFrame
+df = pd.DataFrame(data)
+
+# Print the DataFrame
+print(df)
